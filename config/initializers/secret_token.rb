61a3c1f1a6c3e71ca968a0897cefd31e2fedafe3
Ben Ricker <BGRicker@gmail.com>
2014-11-05 01:26:40 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Keepthisbrief::Application.config.secret_key_base = '3cd7355a4c234dc0778c1def71b4b121627b13a90c8ed4a95daa02992f6f174a6a94967fe4d79f374703dc31f1a0c1d412ded1f7565c6745e7006a7679401438'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Keepthisbrief::Application.config.session_store :cookie_store, key: '_keepthisbrief_session'
