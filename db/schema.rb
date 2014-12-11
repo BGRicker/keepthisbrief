e35388e36cb513e6915627410f712d50eb4fad30
Ben Ricker <BGRicker@gmail.com>
2014-12-11 02:21:55 +0000

    categories migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210024057) do
+ActiveRecord::Schema.define(version: 20141211021458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20141210024057) do
   create_table "categories", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "slug"
   end
 
   create_table "posts", force: true do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.find_or_create_by(:name => "games", :slug => "games")
+Category.find_or_create_by(:name => "movies", :slug => "movies")
+Category.find_or_create_by(:name => "tv", :slug => "tv")
