0ebfe2e7b69745cb9675cbb1fed0822f4e285a72
Ben Ricker <BGRicker@gmail.com>
2014-12-11 02:29:22 +0000

    categories posts logic

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,26 @@
 class Admin::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    Category.create(category_params)
+    redirect_to root_path
+  end
+
+  def current_category
+    @current_category ||= Category.find(params[:id])
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :post_id)
+  end
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,18 @@
 class Category < ActiveRecord::Base
+  validates :slug, uniqueness: true
+  has_many :posts
+
+
+  def self.find_by_slug(slug)
+    Category.where(:slug => slug).first
+  end
+
+  def to_param
+    self.slug
+  end
+
+  def current_category
+    @current_category ||= Category.find(params[:id])
+  end
+
 end
