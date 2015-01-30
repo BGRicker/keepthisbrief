99e3128bdf1d1a041e54410868ee2ce8bf95804b
Ben Ricker <BGRicker@gmail.com>
2015-01-30 02:57:35 +0000

    fix .inspect on likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts
-
-  def to_s
-  end
-
+  has_many :likes
 
   def likes?(post)
     post.likes.where(user_id: id).any?
